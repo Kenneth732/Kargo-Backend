47f7b350208a4113bd4ba30ee5880107a53909ef
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-26 17:49:53 +0300

    auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_26_142333) do
+ActiveRecord::Schema.define(version: 2023_05_26_144156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2023_05_26_142333) do
     t.string "image"
     t.string "category"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,120 @@
 Machine.create([
     {
-        name: "",
+        name: "Emb Art Demention",
         price: 20000,
-        image: "",
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/342bef5c-b8b8-424b-ab0d-fc6bdaa5037b/Leonardo_Diffusion_The_embroidery_machine_is_a_sleek_and_powe_0.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/342bef5c-b8b8-424b-ab0d-fc6bdaa5037b/Leonardo_Diffusion_The_embroidery_machine_is_a_sleek_and_powe_1.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/238f227b-11cc-4da0-957a-3e6c5e611967/Leonardo_Diffusion_The_embroidery_machine_with_four_heads_is_a_1.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/238f227b-11cc-4da0-957a-3e6c5e611967/Leonardo_Diffusion_The_embroidery_machine_with_four_heads_is_a_0.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/95fea558-c029-4a17-94f7-514a4abf12c2/Leonardo_Diffusion_The_embroidery_machine_with_four_heads_is_a_1.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/b5b5ca42-f2e3-4c90-b842-81ccc8ec85ad/Leonardo_Diffusion_The_embroidery_machine_with_four_heads_is_a_1.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/b5b5ca42-f2e3-4c90-b842-81ccc8ec85ad/Leonardo_Diffusion_The_embroidery_machine_with_four_heads_is_a_0.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/cc6f9083-be98-4207-b348-de52ff15b230/Leonardo_Diffusion_dark_background_ui_ux_ecommerce_website_for_1.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/cc6f9083-be98-4207-b348-de52ff15b230/Leonardo_Diffusion_dark_background_ui_ux_ecommerce_website_for_0.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/b19dca9d-858b-4bc2-9a2d-1bf6f30c6730/Leonardo_Diffusion_dark_background_ui_ux_ecommerce_website_for_0.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/b19dca9d-858b-4bc2-9a2d-1bf6f30c6730/Leonardo_Diffusion_dark_background_ui_ux_ecommerce_website_for_1.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/aa5c76e3-79ad-48a3-94d8-2f9500923467/Leonardo_Diffusion_ui_ux_ecommerce_website_for_embroidery_mach_0.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/aa5c76e3-79ad-48a3-94d8-2f9500923467/Leonardo_Diffusion_ui_ux_ecommerce_website_for_embroidery_mach_1.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/1eb4cb1a-7b41-4aeb-b422-6331f73d2989/Leonardo_Diffusion_ui_ux_ecommerce_website_for_embroidery_mach_1.jpg?w=512",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/1eb4cb1a-7b41-4aeb-b422-6331f73d2989/Leonardo_Diffusion_ui_ux_ecommerce_website_for_embroidery_mach_0.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/e6e29c39-dead-4ccb-a30f-62984d435484/Leonardo_Diffusion_ui_ux_ecommerce_website_for_embroidery_mach_0.jpg",
+        description: "",
+        category: ""
+    },
+    {
+        name: "Emb Art Demention",
+        price: 20000,
+        image: "https://cdn.leonardo.ai/users/ca9e5ae6-c2a1-45db-80c6-2bc46654f209/generations/e6e29c39-dead-4ccb-a30f-62984d435484/Leonardo_Diffusion_ui_ux_ecommerce_website_for_embroidery_mach_1.jpg",
         description: "",
         category: ""
     }
