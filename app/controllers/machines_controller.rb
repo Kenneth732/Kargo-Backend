63715d5b6dbc093d4880d8ad89ac3f5570c0cc94
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-26 18:00:53 +0300

    price cal...

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -15,11 +15,26 @@ class MachinesController < ApplicationController
         render json: status: :created
     end
 
-    def update 
-    end
-
-    def destroy
-    end
+    def update
+        machine = Machine.find(params[:id])
+        old_price = machine.price
+        new_price = calculate_new_price(old_price)  # Custom method to calculate the new price
+    
+        if machine.update(price: new_price)
+          # Handle successful update
+        else
+          # Handle update failure
+        end
+      end
+    
+      def destroy
+        cart = Cart.find(params[:id])
+        if cart.destroy
+          # Handle successful deletion
+        else
+          # Handle deletion failure
+        end
+      end
 
 
     private 
@@ -27,5 +42,15 @@ class MachinesController < ApplicationController
     def machine_params
         params.require(:machine).permit(:name, :price, :image, :category, :description)
     end
+
+    def calculate_new_price(old_price)
+        # Perform your calculation logic here based on the old price
+        # For example, let's say we want to increase the price by 10%
+        increase_percentage = 10
+        new_price = old_price + (old_price * increase_percentage / 100)
+      
+        # Return the new calculated price
+        new_price
+      end      
 end
 
