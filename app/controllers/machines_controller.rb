9aa5d904231bb480cd774dc936d80c4dd0bed12c
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-26 17:57:46 +0300

    validates

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,2 +1,31 @@
 class MachinesController < ApplicationController
+
+    def index
+        machines = Machine.all 
+        render json machines 
+    end
+
+    def show
+        machine = Machine.find(params[:id])
+        render json: machine 
+    end
+
+    def create
+        machine = Machine.create(machine_params)
+        render json: status: :created
+    end
+
+    def update 
+    end
+
+    def destroy
+    end
+
+
+    private 
+
+    def machine_params
+        params.require(:machine).permit(:name, :price, :image, :category, :description)
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :users
-  resources :machines
+  resources :machines, only: [:index, :show, :create, :update, :destroy]
+
+  post '/signup', to: 'users#create'
+
+  post "/login", to: "sessions#create"
+  get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
 end
